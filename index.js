1cdddc92853d7b4c9bb221935bd6f34fb5eb7050
mulukimwangeka <mwangekaruth@gmail.com>
2022-09-01 00:07:47 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  
+    const seenNumbers = {};
+  
+    for (const number of array) {
+      // n steps
+      const complement = target - number;
+      if (complement in seenNumbers) return true;
+      seenNumbers[number] = true;
+    }
+  
+    return false;
+  }
+
+// O(n) runtime
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
 }
+
+// O(1) runtime
+function findSock(object) {
+  if (object.sock) return "sock";
+}
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -29,6 +53,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
 }
 
 module.exports = hasTargetSum;
